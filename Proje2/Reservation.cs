6046967d1142aa4bd7c270bb68f3fa7bd07eb4ce
jvoljvolizka <jvoljvolizka@gmail.com>
2019-05-27 19:40:26 -0700

    reserv class

---
+++ Proje2/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2
+{
+    class Reservation
+    {
+        DateTime date;
+        otel place;
+        Oda room;
+        musteri user;
+
+        Reservation()
+        {
+
+        }
+
+        void ShowReserv()
+        {
+
+        }
+    }
+}
