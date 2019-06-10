8a8473195df4a03ebd5179b7ac67d30ad0296e12
jvoljvolizka <jvoljvolizka@gmail.com>
2019-06-10 01:16:46 -0700

    merge test 1

---
+++ Proje2/FileOp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2
+{
+    class FileOp
+    {
+        void filewrite()
+        {
+
+
+        }
+
+        void fileread()
+        {
+
+        }
+
+    }
+}
---
+++ Proje2/SystemControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2
+{
+    class SystemControl
+    {
+        List<Kullanici> userlist;
+        List<otel> otellist;
+
+        internal List<Kullanici> Userlist { get => userlist; set => userlist = value; }
+        internal List<otel> Otellist { get => otellist; set => otellist = value; }
+    }
+}
